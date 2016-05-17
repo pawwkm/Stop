ace65e953f62e274ce0430be9216ed0ff84bf4df
Paw Møller <pawwkm@gmail.com>
2016-05-17 14:12:43 +0200

    Basic setup

---
+++ Stop.FileSystems.Tests/DiskStream.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System.Windows.Forms;
+
+namespace Stop.FileSystems
+{
+    /// <summary>
+    /// Provides tests for the <see cref="DiskStream"/> class.
+    /// </summary>
+    [TestFixture, Explicit]
+    public class DiskStreamTests
+    {
+        /// <summary>
+        /// Figures out which disk to test on.
+        /// </summary>
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            MessageBox.Show("Only once");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void Test1()
+        {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Test]
+        public void Test2()
+        {
+
+        }
+    }
+}
---
+++ Stop.FileSystems/DiskStream.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stop.FileSystems
+{
+    /// <summary>
+    /// Streams the raw contents of a disk.
+    /// </summary>
+    public class DiskStream : Stream
+    {
+        /// <summary>
+        /// The stream supports reading.
+        /// </summary>
+        public override bool CanRead
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// The stream supports seeking.
+        /// </summary>
+        public override bool CanSeek
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// The stream supports reading.
+        /// </summary>
+        public override bool CanWrite
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// The length of the disk in bytes.
+        /// </summary>
+        public override long Length
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// The current position within the stream.
+        /// </summary>
+        /// <exception cref="IOException">
+        /// An I/O error occurs.
+        /// </exception>
+        /// <exception cref="ObjectDisposedException">
+        /// Methods were called after the stream was closed.
+        /// </exception>
+        public override long Position
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        /// <summary>
+        /// Clears all buffers for this stream and causes any 
+        /// buffered data to be written to the underlying device.
+        /// </summary>
+        /// <exception cref="IOException">
+        /// An I/O error occurs.
+        /// </exception>
+        /// <exception cref="ObjectDisposedException">
+        /// Methods were called after the stream was closed.
+        /// </exception>
+        public override void Flush()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Reads a sequence of bytes from the current stream and 
+        /// advances the position within the stream by the number 
+        /// of bytes read.
+        /// </summary>
+        /// <param name="buffer">
+        /// When this method returns, the buffer contains the specified
+        /// byte array with the values between offset and 
+        /// (<paramref name="offset"/> + <paramref name="count"/> - 1) replaced by
+        /// the bytes read from the current source.
+        /// </param>
+        /// <param name="offset">
+        /// Offset in buffer at which to begin storing the data read 
+        /// from the current stream.
+        /// </param>
+        /// <param name="count">The maximum number of bytes to be read from the current stream.</param>
+        /// <returns>
+        /// The total number of bytes read into the buffer. This can be less than the number
+        /// of bytes requested if that many bytes are not currently available, or zero (0)
+        /// if the end of the stream has been reached.
+        /// </returns>
+        /// <exception cref="IOException">
+        /// An I/O error occurs.
+        /// </exception>
+        /// <exception cref="ObjectDisposedException">
+        /// Methods were called after the stream was closed.
+        /// </exception>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Sets the position within the current stream.
+        /// </summary>
+        /// <param name="offset">A byte offset relative to the <paramref name="origin"/>.</param>
+        /// <param name="origin">The reference point used to obtain the new position.</param>
+        /// <returns>The new position within the current stream.</returns>
+        /// <exception cref="IOException">
+        /// An I/O error occurs.
+        /// </exception>
+        /// <exception cref="ObjectDisposedException">
+        /// Methods were called after the stream was closed.
+        /// </exception>
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The physical disk can't be resized and is 
+        /// therefore not supported.
+        /// </summary>
+        /// <param name="value">This value is not used.</param>
+        /// <exception cref="NotSupportedException">
+        /// This is not supported.
+        /// </exception>
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// Writes a sequence of bytes to the current 
+        /// stream and advances the current position within this 
+        /// stream by the number of bytes written
+        /// </summary>
+        /// <param name="buffer">The bytes to write.</param>
+        /// <param name="offset">
+        /// Byte offset in <paramref name="buffer"/> at which to begin 
+        /// copying bytes to the current stream.
+        /// </param>
+        /// <param name="count">
+        /// The number of bytes to be written to the current stream.
+        /// </param>
+        /// <exception cref="IOException">
+        /// An I/O error occurs.
+        /// </exception>
+        /// <exception cref="ObjectDisposedException">
+        /// Methods were called after the stream was closed.
+        /// </exception>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
